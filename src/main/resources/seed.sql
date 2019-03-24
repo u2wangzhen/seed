e8e5f90101ca33046ec0e7bad088f5ff797b847b
u2wangzhen <308968013@qq.com>
2019-03-24 20:52:25 +0800

    sql更新

--- src/main/resources/seed.sql
+++ src/main/resources/seed.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50643
 File Encoding         : 65001
 
-Date: 2019-03-23 20:26:44
+Date: 2019-03-24 20:51:36
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -21,14 +21,77 @@ SET FOREIGN_KEY_CHECKS=0;
 DROP TABLE IF EXISTS `t_fruit`;
 CREATE TABLE `t_fruit` (
   `ID` bigint(11) NOT NULL AUTO_INCREMENT,
-  `F_KEY` varchar(16) DEFAULT NULL,
+  `F_KEY` varchar(32) DEFAULT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of t_fruit
 -- ----------------------------
-INSERT INTO `t_fruit` VALUES ('7', 'user');
+
+-- ----------------------------
+-- Table structure for `t_seed_11`
+-- ----------------------------
+DROP TABLE IF EXISTS `t_seed_11`;
+CREATE TABLE `t_seed_11` (
+  `id` bigint(11) NOT NULL AUTO_INCREMENT,
+  `S_key` varchar(32) DEFAULT NULL,
+  `S_value` varchar(16) DEFAULT NULL,
+  `S_fid` bigint(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
+
+-- ----------------------------
+-- Records of t_seed_11
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `t_seed_128`
+-- ----------------------------
+DROP TABLE IF EXISTS `t_seed_128`;
+CREATE TABLE `t_seed_128` (
+  `id` bigint(11) NOT NULL AUTO_INCREMENT,
+  `S_key` varchar(32) DEFAULT NULL,
+  `S_value` varchar(128) DEFAULT NULL,
+  `S_fid` bigint(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
+
+-- ----------------------------
+-- Records of t_seed_128
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `t_seed_16`
+-- ----------------------------
+DROP TABLE IF EXISTS `t_seed_16`;
+CREATE TABLE `t_seed_16` (
+  `id` bigint(11) NOT NULL AUTO_INCREMENT,
+  `S_key` varchar(32) DEFAULT NULL,
+  `S_value` varchar(16) DEFAULT NULL,
+  `S_fid` bigint(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=43 DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
+
+-- ----------------------------
+-- Records of t_seed_16
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `t_seed_255`
+-- ----------------------------
+DROP TABLE IF EXISTS `t_seed_255`;
+CREATE TABLE `t_seed_255` (
+  `id` bigint(11) NOT NULL AUTO_INCREMENT,
+  `S_key` varchar(32) DEFAULT NULL,
+  `S_value` varchar(255) DEFAULT NULL,
+  `S_fid` bigint(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
+
+-- ----------------------------
+-- Records of t_seed_255
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for `t_seed_32`
@@ -36,16 +99,28 @@ INSERT INTO `t_fruit` VALUES ('7', 'user');
 DROP TABLE IF EXISTS `t_seed_32`;
 CREATE TABLE `t_seed_32` (
   `id` bigint(11) NOT NULL AUTO_INCREMENT,
-  `S_key` varchar(16) DEFAULT NULL,
+  `S_key` varchar(32) DEFAULT NULL,
   `S_value` varchar(32) DEFAULT NULL,
   `S_fid` bigint(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of t_seed_32
 -- ----------------------------
-INSERT INTO `t_seed_32` VALUES ('14', 'type', 'add', '7');
-INSERT INTO `t_seed_32` VALUES ('15', 'name', 'wz', '7');
-INSERT INTO `t_seed_32` VALUES ('16', 'age', '30', '7');
-INSERT INTO `t_seed_32` VALUES ('17', 'password', 'gg', '7');
+
+-- ----------------------------
+-- Table structure for `t_seed_64`
+-- ----------------------------
+DROP TABLE IF EXISTS `t_seed_64`;
+CREATE TABLE `t_seed_64` (
+  `id` bigint(11) NOT NULL AUTO_INCREMENT,
+  `S_key` varchar(32) DEFAULT NULL,
+  `S_value` varchar(64) DEFAULT NULL,
+  `S_fid` bigint(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
+
+-- ----------------------------
+-- Records of t_seed_64
+-- ----------------------------
